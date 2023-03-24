7b351a2a7c44b7588c463a137ee4e2917c4be5cf
KMail <kmailsoftwaredevelopment@gmail.com>
2023-03-24 11:05:08 +0630

    Update

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,6 +2,10 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Contract;
+use App\Models\Demand;
+use App\Models\Interview;
+use App\Models\Sending;
 use Illuminate\Http\Request;
 use LaravelDaily\LaravelCharts\Classes\LaravelChart;
 
@@ -24,8 +28,29 @@ class HomeController extends Controller
      */
     public function index()
     {
+        // Demand Total 
+        $demand_male = Demand::sum('male');
+        $demand_female = Demand::sum('female');
+        $total_demand = $demand_male + $demand_female;
+
+        // Interview Total 
+        $interview_male = Interview::sum('interview_male');
+        $interview_female = Interview::sum('interview_female');
+        $total_interview = $interview_male + $interview_female;
+
+        // Contract 
+        $contract_male = Contract::sum('contract_male');
+        $contract_female = Contract::sum('contract_female');
+        $total_contract = $contract_male + $contract_female;
+
+        // Sending
+        $sending_male = Sending::sum('sending_male');
+        $sending_female = Sending::sum('sending_female');
+        $total_sending = $sending_male + $sending_female;
+
+
         $chart_options = [
-            'chart_title' => 'Daily Labour',
+            'chart_title' => 'Daily Data Entry - Labour',
             'report_type' => 'group_by_date',
             'model' => 'App\Models\Passport',
             'group_by_field' => 'created_at',
@@ -37,6 +62,37 @@ class HomeController extends Controller
 
         $passports_report = new LaravelChart($chart_options);
 
-        return view('home', compact('passports_report'));
+
+        $chart_options = [
+            'chart_title' => 'Medical Test - Failed & Pass',
+            'report_type' => 'group_by_string',
+            'model' => 'App\Models\MedicalTest',
+            'group_by_field' => 'failed_or_pass',
+            'chart_type' => 'pie',
+        ];
+        $medical_test = new LaravelChart($chart_options);
+
+
+        $chart_options = [
+            'chart_title' => 'Male - Female',
+            'report_type' => 'group_by_string',
+            'model' => 'App\Models\Passport',
+            'group_by_field' => 'gender',
+            'chart_type' => 'pie',
+        ];
+        $passport_male_female = new LaravelChart($chart_options);
+
+
+
+        $chart_options = [
+            'chart_title' => 'Male - Female',
+            'report_type' => 'group_by_string',
+            'model' => 'App\Models\Passport',
+            'group_by_field' => 'reject_status',
+            'chart_type' => 'pie',
+        ];
+        $passport_reject = new LaravelChart($chart_options);
+
+        return view('home', compact('total_demand', 'total_interview', 'total_contract', 'total_sending', 'passports_report', 'medical_test', 'passport_male_female', 'passport_reject'));
     }
 }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,14 +1,145 @@
 @extends('layouts.main')
 @section('content')
     <div class="container-xxl flex-grow-1 container-p-y">
-        <div class="row justify-content-center">
+        <div class="row">
+
+            <div class="col-3 mb-4">
+                <div class="card" style="background-color:#fcd733;">
+                    <div class="card-body">
+                        <div class="d-flex justify-content-between" style="position: relative;">
+                            <div class="d-flex align-items-center gap-3">
+                                <div class="avatar">
+                                    <span class="avatar-initial bg-label-primary rounded-circle"><i
+                                            class="bx bx-user fs-4"></i></span>
+                                </div>
+                                <div class="card-info">
+                                    <h5 class="card-title mb-0 me-2">
+                                        {{ $total_demand ?? 0 }}
+                                    </h5>
+                                    <small class="" style="font-size: 16px; color:white; font-weight:bold;">
+                                        Demand(Male + Female)
+                                    </small>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-3 mb-4">
+                <div class="card" style="background-color:#8ecbfc;">
+                    <div class="card-body">
+                        <div class="d-flex justify-content-between" style="position: relative;">
+                            <div class="d-flex align-items-center gap-3">
+                                <div class="avatar">
+                                    <span class="avatar-initial bg-label-primary rounded-circle">
+                                        <i class="fa fa-users fs-4"></i>
+                                    </span>
+                                </div>
+                                <div class="card-info">
+                                    <h5 class="card-title mb-0 me-2">
+                                        {{ $total_interview ?? 0 }}
+                                    </h5>
+                                    <small class="" style="font-size: 16px; color:white; font-weight:bold;">
+                                        Interview(Male + Female)
+                                    </small>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-3 mb-4">
+                <div class="card" style="background-color: #7deb54">
+                    <div class="card-body">
+                        <div class="d-flex justify-content-between" style="position: relative;">
+                            <div class="d-flex align-items-center gap-3">
+                                <div class="avatar">
+                                    <span class="avatar-initial bg-label-primary rounded-circle">
+                                        <i class="fa-solid fa-file-signature fs-4"></i>
+                                    </span>
+                                </div>
+                                <div class="card-info">
+                                    <h5 class="card-title mb-0 me-2">
+                                        {{ $total_contract ?? 0 }}
+                                    </h5>
+                                    <small class="" style="font-size: 16px; color:white; font-weight:bold;">
+                                        Contract (Male + Female)
+                                    </small>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-3 mb-4">
+                <div class="card" style="background-color: #26b7b7;">
+                    <div class="card-body">
+                        <div class="d-flex justify-content-between" style="position: relative;">
+                            <div class="d-flex align-items-center gap-3">
+                                <div class="avatar">
+                                    <span class="avatar-initial bg-label-primary rounded-circle">
+                                        <i class="fa-sharp fa-solid fa-paper-plane fs-4"></i>
+                                    </span>
+                                </div>
+                                <div class="card-info">
+                                    <h5 class="card-title mb-0 me-2">
+                                        {{ $total_sending ?? 0 }}
+                                    </h5>
+                                    <small class="" style="font-size: 16px; color:white; font-weight:bold;">
+                                        Sending (Male + Female)
+                                    </small>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+
 
             <div class="col-12">
                 <div class="card">
-                    <div class="card-body py-5" >
+                    <div class="card-body py-5">
                         <h6>{{ $passports_report->options['chart_title'] }}</h6>
                         <div style="height: 400px;">
                             {!! $passports_report->renderHtml() !!}
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-4 py-3">
+                <div class="card">
+                    <div class="card-body py-5">
+                        <h6>{{ $medical_test->options['chart_title'] }}</h6>
+                        <div style="height: 400px;">
+                            {!! $medical_test->renderHtml() !!}
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-4 py-3">
+                <div class="card">
+                    <div class="card-body py-5">
+                        <h6>{{ $passport_male_female->options['chart_title'] }} </h6>
+                        <div style="height: 400px;">
+                            {!! $passport_male_female->renderHtml() !!}
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+
+            <div class="col-4 py-3">
+                <div class="card">
+                    <div class="card-body py-5">
+                        <h6>{{ $passport_reject->options['chart_title'] }} </h6>
+                        <div style="height: 400px;">
+                            {!! $passport_reject->renderHtml() !!}
                         </div>
                     </div>
                 </div>
@@ -22,5 +153,14 @@
     <script type="text/javascript">
         {!! $passports_report->renderChartJsLibrary() !!}
         {!! $passports_report->renderJs() !!}
+
+        {!! $medical_test->renderChartJsLibrary() !!}
+        {!! $medical_test->renderJs() !!}
+
+        {!! $passport_male_female->renderChartJsLibrary() !!}
+        {!! $passport_male_female->renderJs() !!}
+
+        {!! $passport_reject->renderChartJsLibrary() !!}
+        {!! $passport_reject->renderJs() !!}
     </script>
 @endsection
